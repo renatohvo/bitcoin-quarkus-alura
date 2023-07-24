d684c42a4e53a43a16bcc7bbade15765739af6a8
renatohvo <renatohvo@gmail.com>
2023-07-24 18:04:55 -0300

    project lombok

--- src/main/java/com/renatohvo/model/Bitcoin.java
+++ src/main/java/com/renatohvo/model/Bitcoin.java
@@ -1,7 +1,12 @@
 package com.renatohvo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class Bitcoin {
 
     private Long id;
@@ -12,45 +17,4 @@ public class Bitcoin {
 
     private LocalDate data;
 
-    public Bitcoin(Long id, Double preco, String tipo, LocalDate data) {
-        this.id = id;
-        this.preco = preco;
-        this.tipo = tipo;
-        this.data = data;
-    }
-
-    public Bitcoin() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
 }
