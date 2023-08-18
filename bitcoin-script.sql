dcd31c71e559a72a79880efe43927bb45ebf30cb
renatohvo <renatohvo@gmail.com>
2023-08-18 17:56:21 -0300

    run sql script using docker compose and prometheus.yml

--- bitcoin-script.sql
+++ bitcoin-script.sql
@@ -1,14 +1,15 @@
-CREATE DATABASE bitcoin;
+CREATE DATABASE IF NOT EXISTS bitcoin;
 USE bitcoin;
-CREATE TABLE `Usuario` (
+CREATE TABLE IF NOT EXISTS `Usuario` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
 	`nome` VARCHAR(255) NOT NULL,
 	`cpf` VARCHAR(255) NOT NULL,
 	`username` VARCHAR(255) NOT NULL,
 	`password` VARCHAR(255) NOT NULL,
+	`role` VARCHAR(55) NULL,
 	PRIMARY KEY(`id`)) ENGINE=InnoDB;
 	
-CREATE TABLE `Ordem` (
+CREATE TABLE IF NOT EXISTS `Ordem` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
 	`preco` float(6,2) NOT NULL,
 	`tipo` VARCHAR(255) NOT NULL,
@@ -17,6 +18,3 @@ CREATE TABLE `Ordem` (
 	`user_id` bigint NOT NULL,
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`user_id`) REFERENCES `Usuario` (`id`)) ENGINE=InnoDB;
-
-ALTER TABLE `bitcoin`.`Usuario` 
-ADD COLUMN `role` VARCHAR(55) NULL AFTER `password`;
